5c5951f25c0d9c486c180a25115797252d92860e
Usama Ibraheem <usama-ismail@users.noreply.huggingface.co>
2026-01-31 21:58:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,55 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from playwright.sync_api import sync_playwright
-import random, time, re
+import random
+import time
+import re
 
 app = FastAPI()
 
 MIN_DELAY = 2
 MAX_DELAY = 9
 
+
 def human_delay():
     time.sleep(random.uniform(MIN_DELAY, MAX_DELAY))
+
 
 def clean_amazon_image(url):
     if url:
         return re.sub(r'\._AC_.*?\.', '.', url)
     return url
 
+
 class SearchInput(BaseModel):
-    upcs: list[str]
+    upc: str
+
 
 @app.post("/search")
-def search_products(data: SearchInput):
-    results = []
+def search_product(data: SearchInput):
+
+    upc = data.upc
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         context = browser.new_context(locale="en-US")
         page = context.new_page()
 
-       # for upc in data.upcs:
         try:
-            page.goto(f"https://www.amazon.com/s?k={upc}", timeout=60000)
+            page.goto(
+                f"https://www.amazon.com/s?k={upc}",
+                timeout=60000
+            )
+
             human_delay()
-            
-            item = page.query_selector("div[data-component-type='s-search-result']")
+
+            item = page.query_selector(
+                "div[data-component-type='s-search-result']"
+            )
+
             if not item:
-                continue
+                browser.close()
+                return {"error": "Product not found", "SKU": upc}
 
             asin = item.get_attribute("data-asin")
 
@@ -48,20 +62,21 @@ def search_products(data: SearchInput):
 
             link_el = item.query_selector("h2 a")
             link = (
-                "https://www.amazon.com" + link_el.get_attribute("href")
+                "https://www.amazon.com" +
+                link_el.get_attribute("href")
                 if link_el else ""
             )
 
-                # results.append({
-                #     "SKU": upc,
-                #     "ASIN": asin,
-                #     "Title": title,
-                #     "Image": image,
-                #     "AmazonURL": link
-                # })
-        except Exception as e:
-            print("Error:", upc, e)
-            
-        browser.close()
+            browser.close()
 
-    return {"results": results,"item":item}
+            return {
+                "SKU": upc,
+                "ASIN": asin,
+                "Title": title,
+                "Image": image,
+                "AmazonURL": link
+            }
+
+        except Exception as e:
+            browser.close()
+            return {"error": str(e), "SKU": upc, "item":item}
